1ec3725e2e1ef3be7c4fe63d8e39080cb0dd3d4f
SpiderDead <mimij68@live.nl>
2019-06-25 15:01:02 +0200

    Added Data file storage for playerData

--- listeners/guildMemberAdd.js
+++ listeners/guildMemberAdd.js
@@ -1,8 +1,17 @@
 exports.run = (client, member) => {
+    const fs = require('fs');
+
     const randomIP = require('../utils/IP.js').generateIP();
     const randomIPdashed = randomIP.replace(/[.]/g, '_');
-    let channelid = '';
 
+    if (!fs.existsSync('./data/playerData.json')) {
+        console.log('[WumpusOS] PlayerData file not found. Creating one now...');
+        fs.writeFile('./data/playerData.json', JSON.stringify([]), (err) => {
+            if (err) throw err;
+            console.log('[WumpusOS] PlayerData file created!');
+        });
+
+    }
 
     member.setNickname(randomIP).catch(console.error);
     member.guild.createChannel(randomIPdashed, { type: 'text'})
@@ -19,13 +28,24 @@ exports.run = (client, member) => {
                     }
                 ]}).catch(console.error);
 
-                c.send('**Hello Stranger. Welcome to WumpusOS**\n\n' +
-                    'You are one of the many systems out here. Your goal is to survive. Other systems can break yours,' +
-                    ' but you can also break theirs...\n\n' +
-                    'I am Wumpus, your guide. I will learn you the basics of this game. Just type `help me wumpus` and ' +
-                    'I will instantly give you some basic commands and tutorials!\n\n' +
-                    'Good luck <@'+member.id+'>, I am counting on you.\n\n' +
-                    'O so you know, I gave you the IP: '+randomIP).catch(console.error);
+            fs.readFile('./data/playerData.json', (err, pd) => {
+                if (err) throw err;
+                const playerData = JSON.parse(pd);
+
+                playerData.push({"userid": member.id, "gameid": randomIP, "channelid": c.id});
+
+                fs.writeFile("./data/playerData.json", JSON.stringify(playerData), (err) => {
+                    if (err) throw err;
+                });
+            });
+
+            c.send('**Hello Stranger. Welcome to WumpusOS**\n\n' +
+                'You are one of the many systems out here. Your goal is to survive. Other systems can break yours,' +
+                ' but you can also break theirs...\n\n' +
+                'I am Wumpus, your guide. I will learn you the basics of this game. Just type `help me wumpus` and ' +
+                'I will instantly give you some basic commands and tutorials!\n\n' +
+                'Good luck <@'+member.id+'>, I am counting on you.\n\n' +
+                'O so you know, I gave you the IP: '+randomIP).catch(console.error);
 
         })
         .catch(console.error);
